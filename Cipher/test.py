c0afda1f439466b0c85d73df45be3e1a68dbc08f
Gabriel.Sturtevant <Gabriel@GabrielSturtevant.com>
2016-09-24 18:17:29 -0700

    Changes

--- Cipher/test.py
+++
@@ -1,2 +0,0 @@
-if "blah" in "BLAH":
-	print "True"
---
+++ Cipher/word_organizer.py
@@ -0,0 +1,77 @@
+from popular_words import popular_words
+from operator import itemgetter
+
+
+def organize_words():
+	file_var = open('./pop_words.txt', 'w+')
+	foobar = list(set(popular_words))
+	foobar = sorted(foobar)
+	for x in foobar:
+		x = x.rstrip()
+		var = "\t\t\t\t \"{}\",\n".format(x.upper())
+		file_var.write(var)
+
+
+def organize_simple_list():
+	read_file = open('./FormattedWordList.txt', 'r')
+	write_file = open('./popular_words_copy.txt', 'w+')
+	whole_file = read_file.read()
+	whole_file = whole_file.replace("\'", "").replace('"', '')
+	whole_file = whole_file.split('\n')
+	for x in popular_words:
+		whole_file.append(x)
+	whole_file = list(sorted(set(whole_file)))
+	whole_file = filter(None, whole_file)
+	first = True
+	for x in whole_file:
+		if len(x) > 2:
+			if first:
+				var = 'popular_words = [\"{}\",\n'.format((x.rstrip()).upper())
+				first = False
+			elif x == whole_file[-1]:
+				var = "\t\t\t\t \"{}\"]".format((x.rstrip()).upper())
+			else:
+				var = "\t\t\t\t \"{}\",\n".format((x.rstrip()).upper())
+			write_file.write(var)
+
+
+def organize_results():
+	file_var = open('./results.txt', 'r')
+	num = 1
+	result_set = []
+	items = {}
+	for line in file_var:
+		line = line.rstrip()
+		if num % 2 == 0:
+			some_list = line.split(", ", 2)
+			items['words'] = some_list[0]
+			items['caesar key'] = some_list[1]
+			items['columnar key'] = some_list[2]
+			items_list = [
+				items['number_of_words'],
+				items['string'],
+				items['words'],
+				items['caesar key'],
+				items['columnar key']
+				]
+			result_set.append(items_list)
+			num += 1
+		else:
+			some_list = line.split(" -- ")
+			items['string'] = some_list[0]
+			items['number_of_words'] = int(some_list[1])
+			num += 1
+	result_set = sorted(result_set, key=itemgetter(0), reverse=True)
+	file_var.close()
+	file_var = open('./formatted_results.txt', 'w')
+	for x in result_set:
+		var = "{1}\n{0} -- Number of words\nWords:\n".format(x[0],x[1])
+		for y in x[2].split(" "):
+			if not y == "" and not y == " ":
+				var += "\t{}\n".format(y) 
+		var += "{0}\n{1}\n\n\n".format(x[3].replace(":", " : "), x[4].replace("(", " ").replace(")", ""))
+		file_var.write(var)
+
+
+def foo():
+	organize_results()
